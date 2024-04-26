0f392472f7efe4b72d448b01d63da94e97133baf
RudolfVonStroheim <mgus92896@gmail.com>
2024-04-26 14:17:25 +0300

    Finished converter

--- converter.py
+++ converter.py
@@ -7,6 +7,7 @@ class Converter:
         probe = ffmpeg.probe(f'input/{filename}')
         self.file_data = probe["format"]
         streams = probe["streams"]
+        self.streams_changed = False
         self.video = []
         self.audio = []
         self.vcodec = "copy"
@@ -46,14 +47,12 @@ class Converter:
         command = ['ffmpeg', '-codecs', "f", filename, '-hide_banner']
         result = run(command, capture_output=True, text=True)
         output = result.stdout
-
         codecs = []
         for line in output.split('\n'):
             if line.startswith('.D.V'):
                 parts = line.split()
                 codec = parts[1]
                 codecs.append(codec)
-
                 return codecs
 
     def get_sub_codecs(self, filename):
@@ -72,7 +71,21 @@ class Converter:
         return {"video": self.video, "audio": self.audio, "subs": self.sub}
 
     def change_streams(self, indexes):
-        self.streams_nums = indexes
+        filters = []
+        for stream_type, indexes in indexes.items():
+            for index in indexes:
+                if stream_type == 'video':
+                    filters.append(f'v:{index}')
+                elif stream_type == 'audio':
+                    filters.append(f'a:{index}')
+                elif stream_type == 'subs':
+                    filters.append(f's:{index}')
+        stream = ffmpeg.input(f'input/{self.filename_old}')
+        stream = ffmpeg.filter(stream, 'null', *filters)
+        output_path = f'tmp/{self.filename_old}'
+        stream = ffmpeg.output(stream, output_path, format='null')
+        ffmpeg.run(stream)
+        self.streams_changed = True
 
     def change_video_codec(self, codec):
         if codec in self.video_codecs:
@@ -97,18 +110,45 @@ class Converter:
                 self.vcodec = vcodecs[0]
             self.video_codecs = vcodecs
         else:
-            pass
+            vcodec = self.video[0]["codec_name"]
+            vcodecs = self.get_video_codecs(self.new_format)
+            if vcodec not in vcodecs:
+                self.vcodec = vcodecs[0]
+            self.video_codecs = vcodecs
+
         if self.acodec != "copy":
             acodecs = self.get_audio_codecs(self.new_format)
             if self.acodec not in acodecs:
                 self.acodec = acodecs[0]
             self.audio_codecs = acodecs
         else:
-            pass
+            acodec = self.audio[0]["codec_name"]
+            acodecs = self.get_audio_codecs(self.new_format)
+            if acodec not in acodecs:
+                self.acodec = acodecs[0]
+            self.audio_codecs = acodecs
         if self.scodec != "copy":
             scodecs = self.get_sub_codecs(self.new_format)
             if self.scodec not in scodecs:
                 self.scodec = scodecs[0]
             self.sub_codecs = scodecs
         else:
-            pass
+            scodec = self.sub[0]["codec_name"]
+            scodecs = self.get_sub_codecs(self.new_format)
+            if scodec not in scodecs:
+                self.scodec = scodecs[0]
+            self.sub_codecs = scodecs
+
+    def process(self):
+        if self.streams_changed:
+            path = f'tmp/{self.filename_old}'
+        else:
+            path = f'inp/{self.filename_old}'
+        output_filename = self.filename_old.split(".")[:-1] + self.new_format
+        output_path = f'out/"{output_filename}"'
+        stream = ffmpeg.input(f'input/"{self.filename_old}"')
+        stream = ffmpeg.output(stream, output_path, vcodec=self.vcodec, acodec=self.acodec, scodec=self.scodec)
+        ffmpeg.run(stream)
+
+        return output_filename
+
