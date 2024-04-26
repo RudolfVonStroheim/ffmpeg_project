40142e7374d15cb60bb28efb2991e0fe1ea97230
RudolfVonStroheim <mgus92896@gmail.com>
2024-04-26 11:56:15 +0300

    Update converter

--- converter.py
+++ converter.py
@@ -9,24 +9,106 @@ class Converter:
         streams = probe["streams"]
         self.video = []
         self.audio = []
+        self.vcodec = "copy"
+        self.acodec = "copy"
+        self.scodec = "copy"
         self.sub = []
         for stream in streams:
             st_type = stream["codec_type"]
             match st_type:
                 case "video":
-                    info = {"index": stream["index"], "codec_name": stream["codec_name"]}
                     self.video.append(stream)
                 case "audio":
                     self.audio.append(stream)
                 case "subtitle":
                     self.sub.append(stream)
         self.update_codecs(filename.split('.')[-1])
-        self.orig_form = filename.split('.')[-1]
+        self.filename_old = filename
 
-    def update_codecs(filename):
+    def update_codecs(self, filename):
         self.audio_codecs = self.get_audio_codecs(filename)
         self.video_codecs = self.get_video_codecs(filename)
         self.sub_codecs = self.get_sub_codecs(filename)
 
-    def get_audio_codecs(filename):
-        cm
+    def get_audio_codecs(self, filename):
+        command = ['ffmpeg', '-codecs', "-f", filename, '-hide_banner']
+        result = run(command, capture_output=True, text=True)
+        output = result.stdout
+        codecs = []
+        for line in output.split('\n'):
+            if line.startswith('.D.A'):
+                parts = line.split()
+                codec = parts[1]
+                codecs.append(codec)
+                return codecs
+
+    def get_video_codecs(self, filename):
+        command = ['ffmpeg', '-codecs', "f", filename, '-hide_banner']
+        result = run(command, capture_output=True, text=True)
+        output = result.stdout
+
+        codecs = []
+        for line in output.split('\n'):
+            if line.startswith('.D.V'):
+                parts = line.split()
+                codec = parts[1]
+                codecs.append(codec)
+
+                return codecs
+
+    def get_sub_codecs(self, filename):
+        command = ['ffmpeg', '-codecs', "-f", filename, '-hide_banner']
+        result = run(command, capture_output=True, text=True)
+        output = result.stdout
+        codecs = []
+        for line in output.split('\n'):
+            if line.startswith('.D.S'):
+                parts = line.split()
+                codec = parts[1]
+                codecs.append(codec)
+                return codecs
+
+    def get_streams(self):
+        return {"video": self.video, "audio": self.audio, "subs": self.sub}
+
+    def change_streams(self, indexes):
+        self.streams_nums = indexes
+
+    def change_video_codec(self, codec):
+        if codec in self.video_codecs:
+            self.vcodec = codec
+    
+    def change_audio_codec(self, codec):
+        if codec in self.audio_codecs:
+            self.acodec = codec
+
+    def change_sub_codec(self, codec):
+        if codec in self.sub_codecs:
+            self.scodec = codec
+
+    def change_format(self, new_format):
+        self.new_format = new_format
+        self.check_codecs()
+
+    def check_codecs(self):
+        if self.vcodec != "copy":
+            vcodecs = self.get_video_codecs(self.new_format)
+            if self.vcodec not in vcodecs:
+                self.vcodec = vcodecs[0]
+            self.video_codecs = vcodecs
+        else:
+            pass
+        if self.acodec != "copy":
+            acodecs = self.get_audio_codecs(self.new_format)
+            if self.acodec not in acodecs:
+                self.acodec = acodecs[0]
+            self.audio_codecs = acodecs
+        else:
+            pass
+        if self.scodec != "copy":
+            scodecs = self.get_sub_codecs(self.new_format)
+            if self.scodec not in scodecs:
+                self.scodec = scodecs[0]
+            self.sub_codecs = scodecs
+        else:
+            pass
